b3e0c3c94d8316db211625b97590c98a48920c71
Seohyeon <0205lucy@naver.com>
2022-09-21 19:20:00 +0900

    :sparkles: 2주차

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
-import RefPrac from './week-02/RefPrac';
-import UseStatePrac from './week-01/UseStatePrac';
-import UseEffectPrac from './week-02/UseEffectPrac';
-
+import "./App.css";
+import RefPrac from "./week-02/RefPrac";
+import UseStatePrac from "./week-01/UseStatePrac";
+import UseEffectPrac from "./week-02/UseEffectPrac";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <>
+	return (
+		<>
         <UseEffectPrac/>
-    </>
-  );
+		</>
+
+	);
 }
 
 export default App;
--- src/week-02/UseEffectPrac.js
+++ src/week-02/UseEffectPrac.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const UseEffectPrac = () => {
+    const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
+    const handleClick = () => {
+        setCount(count+1);
+    }
+    
+
+    const handleInputChange = (e) => {
+        setName(e.target.value);
+    }
+    //렌더링 될때마다 매번 실행됨
+    useEffect(()=>{
+
+    },[count])
 	return (
 		<>
-			<div>UseEffectPrac</div>
+        <h1>UseEffect 뿌셔!</h1>
+			<div>UseEffect는 인자로 콜백함수를 기본으로 받음 -> 매번 렌더링 </div>
+            <div>UseEffect가 두번째 인자로 배열을 받을 경우 -> 화면에 첫 렌더링될때, value값이 바뀔 때만 실행  </div>
+            <hr/>
+            <div>count:{count}</div>
+            <button onClick={handleClick}>고고씽</button>
+            <input type="text" value={name} onChange={handleInputChange}/>
+            <div>name: {name}</div>
 		</>
 	);
 };
